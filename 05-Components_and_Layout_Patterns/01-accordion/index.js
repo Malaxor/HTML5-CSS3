7f9f8bd29eb41cb7ba9590ada5b4fd844b76ae2c
Malaxor <nogobaduk@gmail.com>
2021-09-28 17:20:43 -0400

    omnifood project: testimonial (part 1)

--- 05-Components_and_Layout_Patterns/01-accordion/index.js
+++ 05-Components_and_Layout_Patterns/01-accordion/index.js
@@ -2,6 +2,15 @@ const accordion = document.querySelector('.accordion');
 
 accordion.addEventListener('click', e => {
    if(e.target.classList.contains('icon')) {
-      e.target.parentElement.classList.toggle('open');
+      const { target: arrowIcon } = e;
+      arrowIcon.parentElement.classList.toggle('open');
+      const { display: hiddenBoxDisplay } = window.getComputedStyle(e.target.nextElementSibling);
+
+      if(hiddenBoxDisplay === 'block') {
+         arrowIcon.style.transform = `rotate(180deg)`;
+      }
+      else if(hiddenBoxDisplay === 'none') {
+         arrowIcon.style.transform = `rotate(0deg)`
+      }
    }
 });

ff840a82946f92a4fb58e9504ace6182b41614da
Malaxor <nogobaduk@gmail.com>
2021-09-30 15:16:17 -0400

    omnifood project: call to action (part 1)

--- 05-Components_and_Layout_Patterns/01-accordion/index.js
+++ 05-Components_and_Layout_Patterns/01-accordion/index.js
@@ -3,14 +3,11 @@ const accordion = document.querySelector('.accordion');
 accordion.addEventListener('click', e => {
    if(e.target.classList.contains('icon')) {
       const { target: arrowIcon } = e;
-      arrowIcon.parentElement.classList.toggle('open');
-      const { display: hiddenBoxDisplay } = window.getComputedStyle(e.target.nextElementSibling);
 
-      if(hiddenBoxDisplay === 'block') {
-         arrowIcon.style.transform = `rotate(180deg)`;
+      arrowIcon.parentElement.classList.toggle('open');
+      if(arrowIcon.parentElement.classList.contains('open')) {
+         return arrowIcon.style.transform = 'rotate(180deg)';
       }
-      else if(hiddenBoxDisplay === 'none') {
-         arrowIcon.style.transform = `rotate(0deg)`
-      }
+      return arrowIcon.style.transform = 'rotate(0deg)';
    }
 });

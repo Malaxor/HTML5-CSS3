36c7a0e76eb3ca894a4573f3ccf6f8f894891a8d
Malaxor <nogobaduk@gmail.com>
2021-10-11 17:02:33 -0400

    omnifood project: media queries (small tablets)

--- 05-Components_and_Layout_Patterns/01-accordion/index.js
+++ 05-Components_and_Layout_Patterns/01-accordion/index.js
@@ -2,12 +2,11 @@ const accordion = document.querySelector('.accordion');
 
 accordion.addEventListener('click', e => {
    if(e.target.classList.contains('icon')) {
-      const { target: arrowIcon } = e;
-
-      arrowIcon.parentElement.classList.toggle('open');
-      if(arrowIcon.parentElement.classList.contains('open')) {
-         return arrowIcon.style.transform = 'rotate(180deg)';
+      e.target.parentElement.classList.toggle('open');
+      
+      if(e.target.parentElement.classList.contains('open')) {
+         return e.target.style.transform = 'rotate(180deg)';
       }
-      return arrowIcon.style.transform = 'rotate(0deg)';
+      return e.target.style.transform = 'rotate(0deg)';
    }
 });

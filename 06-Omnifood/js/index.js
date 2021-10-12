bfcf3b5b905a99eef5f9c218cfe83c5fef1c21de
Malaxor <nogobaduk@gmail.com>
2021-10-12 14:58:31 -0400

    smooth scroll and closing mobile nav on link click

--- 06-Omnifood/js/index.js
+++ 06-Omnifood/js/index.js
@@ -1,6 +1,32 @@
 const headerEl = document.querySelector('.header');
-const btnNav = document.querySelector('.btn-mobile-nav');
+const btnNavEl = document.querySelector('.btn-mobile-nav');
 
-btnNav.addEventListener('click', () => {
+btnNavEl.addEventListener('click', () => {
    headerEl.classList.toggle('nav-open');
+});
+
+// smooth scroll animation
+const allLinks = document.getElementsByTagName('a');
+
+Array.from(allLinks).forEach(link => {
+   link.addEventListener('click', e => {
+      e.preventDefault();
+      const href = link.getAttribute('href');
+      // scroll back to top
+      if(href === '#') {
+         window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+         })
+      }
+      // links with hrefs pointing to sections (ex: #meals, #cta)
+      if(href.startsWith("#") && href.length > 1) {
+         const sectionEl = document.querySelector(href);
+         sectionEl.scrollIntoView({ behavior: 'smooth' });
+      }
+      // close mobile nav
+      if(link.classList.contains('main-nav-link')) {
+         headerEl.classList.toggle('nav-open');
+      }
+   });
 });

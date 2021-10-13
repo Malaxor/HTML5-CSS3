e2aea659aadd834c6ded2122d7c081e419b13482
Malaxor <nogobaduk@gmail.com>
2021-10-13 14:03:04 -0400

    omnifood project: fixed flexbox gap missing on some Safari versions

--- 06-Omnifood/js/index.js
+++ 06-Omnifood/js/index.js
@@ -1,34 +1,31 @@
 const headerEl = document.querySelector('.header');
 const btnNavEl = document.querySelector('.btn-mobile-nav');
-const allLinks = document.getElementsByTagName('a');
 const sectionHero = document.querySelector('.section-hero');
 
 btnNavEl.addEventListener('click', () => {
    headerEl.classList.toggle('nav-open');
 });
 
-// smooth scroll animation
-Array.from(allLinks).forEach(link => {
-   link.addEventListener('click', e => {
+// smooth scroll animation (event delegation)
+document.body.addEventListener('click', e => {
+   if(e.target.hasAttribute('href')) { 
       e.preventDefault();
-      const href = link.getAttribute('href');
-      // scroll back to top
+      const href = e.target.getAttribute('href');
+   
       if(href === '#') {
          window.scrollTo({
-            top: 0,
-            behavior: 'smooth'
-         })
+            top: 0, 
+            behavior: 'smooth' 
+         });
       }
-      // links with hrefs pointing to sections (ex: #meals, #cta)
-      if(href.startsWith("#") && href.length > 1) {
+      else if(href.startsWith('#') && href.length > 1) {
          const sectionEl = document.querySelector(href);
          sectionEl.scrollIntoView({ behavior: 'smooth' });
       }
-      // close mobile nav
-      if(link.classList.contains('main-nav-link')) {
+      if(e.target.classList.contains('main-nav-link')) {
          headerEl.classList.toggle('nav-open');
       }
-   });
+   }
 });
 // sticky nav
 const obs = new IntersectionObserver(entries => {
@@ -37,7 +34,7 @@ const obs = new IntersectionObserver(entries => {
    if(!entry.isIntersecting) {
       document.body.classList.add('sticky');
    }
-   if(entry.isIntersecting) {
+   else {
       document.body.classList.remove('sticky');
    }
 }, {
@@ -45,7 +42,27 @@ const obs = new IntersectionObserver(entries => {
       root: null,
       // event will fire when 0% of the hero section is in the viewport
       threshold: 0,
+      // event will fire at -80px from bottom of 0% threshold
       rootMargin: '-80px'
    }
 );
 obs.observe(sectionHero);
+
+// fixing flexbox gap property missing in some Safari versions
+(function checkFlexGap() {
+   var flex = document.createElement('div');
+   flex.style.display = 'flex';
+   flex.style.flexDirection = 'column';
+   flex.style.rowGap = '1px';
+
+   flex.appendChild(document.createElement('div'));
+   flex.appendChild(document.createElement('div'));
+
+   document.body.appendChild(flex);
+   var isSupported = flex.scrollHeight === 1;
+   flex.parentNode.removeChild(flex);
+
+   if(!isSupported) {
+      document.body.classList.add('no-flexbox-gap');
+   }
+})();
